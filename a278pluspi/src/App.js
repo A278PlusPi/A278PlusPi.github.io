89962f88f8a5bf1b8857f6462ca5eb58086e4628
A278PlusPi <anthonyjohn.regner61@myhunter.cuny.edu>
2023-08-17 20:05:20 -0400

    Added some initial buttons

---
+++ a278pluspi/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="button_div">
+        <button>1a</button>
+        <button>1b</button>
+        <button>1c</button>
+        <button>2a</button>
+        <button>2b</button>
+        <button>2c</button>
+        <button>3a</button>
+        <button>3b</button>
+        <button>3c</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
