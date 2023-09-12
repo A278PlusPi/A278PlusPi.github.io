dbdf15fd8ac6923106f803f272d2bf13ff85c676
A278PlusPi <anthonyjohn.regner61@myhunter.cuny.edu>
2023-09-12 17:05:14 -0400

    Adding the "coming soon" label to the buttons as the new site is under construction

--- a278pluspi/src/App.js
+++ a278pluspi/src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      Hello and welcome all to my site! Here you can explore many of the best aspects of “me” by clicking one of the buttons below.
       <div className="button_div">
-        <button className="button_red">1a</button>
-        <button className="button_red">1b</button>
-        <button className="button_red">1c</button>
-        <button className="button_blue">2a</button>
-        <button className="button_blue">2b</button>
-        <button className="button_blue">2c</button>
-        <button className="button_orange">3a</button>
-        <button className="button_orange">3b</button>
-        <button className="button_orange">3c</button>
+        <button className="button_red">About me<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_red">My projects and work experience<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_red">My course history<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_blue">My mathematical stuff<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_blue">Links to useful websites<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_blue">Miscellaneous<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_orange">My branding (A278+π)<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_orange">Site updates<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        <button className="button_orange">Contact me<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
       </div>
     </div>
   );
