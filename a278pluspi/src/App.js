cd64d815f8d0abca9c02e55f3bd243ee9292b009
A278PlusPi <anthonyjohn.regner61@myhunter.cuny.edu>
2023-09-18 17:52:49 -0400

    Intro page done! React Router implementation to follow!

--- a278pluspi/src/App.js
+++ a278pluspi/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+/*
 import Bio from './components/Bio';
 import Portfolio from './components/Portfolio';
 import Education from './components/Education';
@@ -11,21 +12,24 @@ import Updates from './components/Updates';
 import Contact from './components/Contact';
 
 import {Routes, Route, Outlet, Link} from "react-router-dom";
+*/
 
 function App() {
   return (
     <div className="App">
       Hello and welcome all to my site! Here you can explore many of the best aspects of “me” by clicking one of the buttons below.
-      <div className="button_div">
-        <button className="button_red">About me<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_red">My projects and work experience<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_red">My course history<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_blue">My mathematical stuff<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_blue">Links to useful websites<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_blue">Miscellaneous<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_orange">My branding (A278+π)<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_orange">Site updates<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
-        <button className="button_orange">Contact me<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+      <div className="button_container">
+        <div className="button_set">
+          <button className="button_red">About me<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_red">My projects and work experience<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_red">My course history<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_blue">My mathematical stuff<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_blue">Links to useful websites<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_blue">Miscellaneous<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_orange">My branding (A278+π)<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_orange">Site updates<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+          <button className="button_orange">Contact me<br/><span style = {{fontSize:"12px"}}>Coming soon</span></button>
+        </div>
       </div>
     </div>
   );
